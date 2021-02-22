a89250844cf6c504a589470b00ffb260853c1368
neze14 <72756219+neze14@users.noreply.github.com>
2021-02-22 16:09:44 +0100

    second commit

--- src/app.service.ts
+++ src/app.service.ts
@@ -5,4 +5,17 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
+
+  getHello2(): {} {
+    return {message: 'Hello World!', title: 'My Greeting App'};
+  }
+
+  getHome(): {} {
+    return {title: 'Mary Jones Nigeria Limited'};
+  }
+
+  getAboutUs(): {} {
+    return {title: 'About Us Page'};
+  }
+
 }
